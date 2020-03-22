fb20656ac839b78b6a5967c0206c68bbb1c1197d
Emilien Devos <contact@emiliendevos.be>
2020-03-22 12:59:47 +0100

    ext4

--- vm/init.sh
+++ vm/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ ! -d /data/git ] ; then
-    git clone https://github.com/Alteiria/dynamicDigitalocean.git /data/git
+if [ ! -d /mnt/git ] ; then
+    git clone https://github.com/Alteiria/dynamicDigitalocean.git /mnt/git
 fi
